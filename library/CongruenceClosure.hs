2781202920af08b61a9fd5e9b260dc2fffceaead
cyril <cyfah100@hhu.de>
2023-03-08 19:29:27 +0100

    Add E-Graph rep for SQL Ast

--- library/CongruenceClosure.hs
+++ library/CongruenceClosure.hs
@@ -1,16 +1,29 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE UndecidableInstances #-}
 {-# LANGUAGE LambdaCase #-}
 -- | Use congruence closure+rewrite rules to reason about SQL queries
 module CongruenceClosure where
 
 
-import qualified Data.Map.Strict as M
 import Control.Monad.State
 
-import Data.Equality.Matching.Database
-import Data.Equality.Matching
+import Data.Equality.Graph.Monad
 
 import qualified CompileQuery as Q
-import qualified SQL
+import qualified Control.Monad.State as S
+import Data.Equality.Analysis (Analysis)
+import Data.Equality.Graph (Language, ClassId, ENode(..))
+import Data.Ord.Deriving (deriveOrd1)
+import Data.Eq.Deriving (deriveEq1)
+import Data.Coerce (coerce)
+import SQL
+import Control.Lens hiding (op)
+import Control.Monad.Reader
+import Control.Monad.Trans.Elevator
+import qualified Data.Map.Strict as M
+import Data.Maybe (catMaybes)
 
 
 -- [Note: Why E-Graphs]
@@ -61,16 +74,138 @@ import qualified SQL
 --
 
 
+newtype EggT anl l a = MonadEgg { unMonadEgg :: StateT Int (EGraphM anl l ) a }
+    deriving (Functor, Applicative, Monad)
+
+class (Monad m, Analysis anl l, Language l) => MonadEgg anl l m | m -> anl l where
+    liftEgg :: EGraphM anl l a -> m a
+    genVar :: m ClassId
+    genSkolem :: String -> m SkolemIdent
+    mergeVars :: ClassId -> ClassId -> m ()
+
+deriving  via Elevator (ReaderT r) m instance MonadEgg anl l m => MonadEgg anl l (ReaderT r m)
+instance (MonadTrans t, Monad (t m), MonadEgg anl l m) => MonadEgg anl l (Elevator t m) where
+    liftEgg = lift . liftEgg
+    genVar = lift genVar
+    genSkolem = lift . genSkolem
+    mergeVars = (lift .) . mergeVars
+
 data SomeValue a
     = Name Q.Var
-    | Proj Q.Var SQL.AField
+    | Proj a SQL.AField
     | SkolemFun SkolemIdent [a]
+    | Fun SkolemIdent [a]
     | Joined a a
     | JoinProj1 a
     | JoinProj2 a
     | Try a
-
+    deriving (Eq, Ord, Show, Functor, Foldable, Traversable)
 type SkolemIdent = String
+
+deriveEq1   ''SomeValue
+deriveOrd1  ''SomeValue
+instance Language SomeValue where
+instance (Analysis l SomeValue) => MonadEgg l SomeValue (EggT l SomeValue) where
+    liftEgg = MonadEgg . lift
+    genVar = MonadEgg $ do
+        i <- S.get
+        S.put (i+1)
+        lift $ add (coerce (Name (Q.Var i "v") :: SomeValue ClassId))
+    genSkolem tag = MonadEgg $ do
+        i <- S.get
+        S.put (i+1)
+        pure (tag <> show i)
+    mergeVars l r = liftEgg (void $ merge l r)
+
+addTerm :: (MonadEgg l SomeValue m) => SomeValue ClassId -> m ClassId
+addTerm = liftEgg . add . coerce
+
+alignFields :: (Monad m, Foldable t, MonadEgg anl SomeValue m) => t AField -> ClassId -> ClassId -> m ()
+alignFields fields l r = 
+  forM_ fields $ \field -> do
+      lf <- addTerm (Proj l field)
+      rf <- addTerm (Proj r field)
+      mergeVars lf rf
+
+runToGraph :: SQL -> (([AField], ClassId), EGraph () SomeValue)
+runToGraph sql = egraph $ evalStateT (unMonadEgg (runReaderT (makeGraph sql) mempty)) 0
+
+mkFun :: (MonadEgg l SomeValue m) => String -> [ClassId] -> m ClassId
+mkFun tag args = addTerm (Fun tag args)
+mkSkolemFun :: (MonadEgg l SomeValue m) => String -> [ClassId] -> m ClassId
+mkSkolemFun tag args = do
+    sk <- genSkolem tag
+    addTerm (SkolemFun sk args)
+makeGraph :: (MonadReader (M.Map Q.Var ClassId) m, MonadEgg l SomeValue m) => SQL -> m ([AField], ClassId)
+makeGraph (OrderBy _ spj) = makeGraph spj
+makeGraph (GroupQ groupBys spj) = do
+   groupBysI <- traverse mkExpr groupBys
+   out <- mkSkolemFun "group_fundep" groupBysI
+   (fields, inner) <- makeGraph spj
+   alignFields fields out inner
+   pure (fields, out)
+makeGraph (Table meta name) = do
+    out <- addTerm (SkolemFun name [])
+    forM_ (coerce (Q.fundeps meta) :: [[String]]) $ \fd -> do
+       args <- traverse addTerm (Proj out <$> fd)
+       fun <- mkSkolemFun ("fd_" <> name) args
+       mergeVars out fun
+    pure (Q.fields meta, out)
+makeGraph (Slice _ _ spj) = makeGraph spj
+makeGraph (DistinctQ spj) = do
+   (fields, inner) <- makeGraph spj
+   innerFields <- traverse addTerm (Proj inner <$> fields)
+   out <- mkSkolemFun "distinct" innerFields
+   alignFields fields out inner
+   pure (fields, out)
+makeGraph (ASPJ (SPJ {sources, wheres, proj})) = do
+    outs <- traverseOf (each . _2) makeGraph sources
+    let bindings = M.fromList [(name, out) | (name, (_, out)) <- outs]
+    joinResult <- mkSkolemFun "join" (M.elems bindings)
+    local (M.union bindings) $ do
+        wheresI <- traverse mkWhere wheres
+        projI <- traverse (mkExprQ joinResult) proj
+        forM_ (M.toList projI) $ \(name, out) -> do
+            t <- addTerm (Proj out name)
+            mergeVars out t
+        out <- mkFilter (catMaybes wheresI) joinResult
+        pure (M.keys proj, out)
+
+mkFilter :: MonadEgg l SomeValue m => [ClassId] -> ClassId -> m ClassId
+mkFilter [] p = pure p 
+mkFilter _ _ = error "Todo: Add non-equijoins"
+    
+mkExprQ :: (MonadEgg l SomeValue m, MonadReader (M.Map Q.Var ClassId) m) => ClassId -> Expr -> m ClassId
+mkExprQ ctx (AggrOp op _) = mkSkolemFun (show op) [ctx]
+mkExprQ _ a = mkExpr a
+
+mkExpr :: (MonadEgg l SomeValue m, MonadReader (M.Map Q.Var ClassId) m) => Expr -> m ClassId
+mkExpr (Singular q) = snd <$> makeGraph q
+mkExpr (Lit l) = mkFun (show l) []
+mkExpr (BOp op l r) = do
+   l' <- mkExpr l
+   r' <- mkExpr r
+   mkFun (show op) [l', r']
+mkExpr (Ref v proj) = do
+   asks (M.lookup v) >>= \case
+       Nothing -> error $ "Unknown variable: " <> show v
+       Just x -> addTerm (Proj x proj)
+mkExpr (AggrOp op _) = error $ "AggrOp not allowed here: " <> show op
+
+
+-- TODO: Track whether this is a non-null context before doing this transformation
+mkWhere :: (MonadEgg l SomeValue m, MonadReader (M.Map Q.Var ClassId) m) => Expr -> m (Maybe ClassId)
+mkWhere (BOp Q.Eql l r) = do
+    li <- mkExpr l
+    ri <- mkExpr r
+    mergeVars li ri
+    pure Nothing
+mkWhere o = do
+    o' <- mkExpr o
+    pure (Just o')
+
+-- deriveEq1   ''SymExpr
+-- deriveOrd1  ''SymExpr
 
 -- data PatEnv lang = PatEnv { uniqSupply :: Int, matchLang :: M.Map (lang Var) Var }
 -- type M lang =  State (PatEnv lang)
